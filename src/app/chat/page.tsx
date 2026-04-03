41627160dc4a4066954777991a3fa2b50b2be492
lauro <laureano.g.t@outlook.com>
2026-04-02 20:16:21 -0600

    Update chat background image and add new Aztec chat image

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -3,7 +3,7 @@
 export default function ChatPage() {
   return (
     <div className="h-screen w-full bg-[#111] flex flex-col relative overflow-hidden bg-no-repeat bg-center"
-         style={{ backgroundImage: 'url(/chat2_azteq.png)', backgroundSize: '100% 100%' }}>
+         style={{ backgroundImage: 'url(/aztec_chat.png)', backgroundSize: '100% 100%' }}>
       {/* Contenedor posicionado en la parte inferior para no tapar el rostro del Azteca */}
       <div className="relative z-10 flex-1 flex flex-col items-center justify-end pb-16 md:pb-24 px-4">
          <ChatClient />
