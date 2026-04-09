6174d406e12abb77a17ae6d35c3d60bba4b30c35
lauro <laureano.g.t@outlook.com>
2026-04-08 18:43:24 -0600

    feat: add TypeScript error suppression for turbopack configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
+    // @ts-expect-error - Desactivamos el aviso de TypeScript en la compilación de Vercel
     turbopack: {
       root: '../',
     },
